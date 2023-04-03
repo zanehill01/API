e306cbec3df905cddf109ee586b5420081776da5
zanehill01 <zane_hill1@baylor.edu>
2023-04-03 14:11:54 -0500

    updates

--- 1_python_repos_start_file.py
+++ 1_python_repos_start_file.py
@@ -67,22 +67,22 @@ from plotly import offline
 
 data = [
     {
-    'type':'bar',
-    'x': repos,
-    'y': stars,
-    'marker':{
-        'color':'rgb(60,100,150)'
-        'line':{'width':1.5, 'color':'rgb(25,25,25)'}
-    }
-    'opacity':0.6:
+        'type':'bar',
+        'x': repos,
+        'y': stars,
+        'marker':{
+            'color':'rgb(60,100,150)',
+            'line':{'width':1.5, 'color':'rgb(25,25,25)'},
+        },
+        "opacity":0.6,
 
     }
 ]
 
 my_layout = {
-    'title':'Most Starred Python Repos on GitHub'
-    'xaxis':{'title':'repository'}
-    'yaxis':{'title':'stars'}
+    'title':'Most Starred Python Repos on GitHub',
+    'xaxis':{'title':'repository'},
+    'yaxis':{'title':'stars'},
 }
 
 fig = {'data':data, 'layout': my_layout}
